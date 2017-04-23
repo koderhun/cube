50e3577109a9deaa0c43b0cd16899ba9d7aff8c2
Ramil <vafarulez@ya.ru>
2017-04-23 11:22:51 +0300

    работающий переключатили куба

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,12 +4,18 @@ var itemClass = 'slide-control__item';
 var $btn = document.querySelectorAll('.' + itemClass);
 var $cube = document.querySelector('.cube');
 
-for (var i; i < $btn.length; i++) {
-    $btn.addEventListener('click', function (event) {
+for (var i = 0; i < $btn.length; i++) {
+    $btn[i].addEventListener('click', function (event) {
         console.log('test');
         $cube.classList.value = 'cube';
-        if (this.classList.contains(itemClass)) {
-            $cube.classList.add(itemClass + '_btn1');
+        if (this.classList.contains('slide-control__item_btn1')) {
+            $cube.classList.add('cube_btn1');
+        } else if (this.classList.contains('slide-control__item_btn2')) {
+            $cube.classList.add('cube_btn2');
+        } else if (this.classList.contains('slide-control__item_btn3')) {
+            $cube.classList.add('cube_btn3');
+        } else if (this.classList.contains('slide-control__item_btn4')) {
+            $cube.classList.add('cube_btn4');
         }
         return false;
     });
